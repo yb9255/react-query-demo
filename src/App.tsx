6633c91d5d539ae54ccc129baebfaab5b17d517c
yoobin <yoobin9255@gmail.com>
2023-07-09 09:43:08 +0900

    add rq devtolls

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import HomePage from "./components/Home.page";
 import RQSuperHeroesPage from "./components/RQSuperHeroes.page";
 import SuperHeroesPage from "./components/SuperHeroes.page";
 import { QueryClientProvider, QueryClient } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 
 const queryClient = new QueryClient();
 
@@ -32,6 +33,7 @@ function App() {
           </Routes>
         </div>
       </Router>
+      <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
     </QueryClientProvider>
   );
 }
